25a5aadc615d56c1c9054b590eaa090cc4477f37
mMahnoor <mahnurakther@gmail.com>
2023-12-16 14:45:10 +0600

    added navbar, home page

---
+++ src/home.jsx
@@ -0,0 +1,130 @@
+import {
+    Typography,
+    Card,
+  } from "@material-tailwind/react";
+
+const Home = () => {
+    return (
+        <div className="mx-auto max-w-screen-md py-12">
+            <Card className="mb-12 overflow-hidden">
+            <img
+                alt="nature"
+                className="h-[32rem] w-full object-cover object-center"
+                src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
+            />
+            </Card>
+            <Typography variant="h2" color="blue-gray" className="mb-2">
+            What is Material Tailwind
+            </Typography>
+            <Typography color="gray" className="font-normal">
+            Can you help me out? you will get a lot of free exposure doing this
+            can my website be in english?. There is too much white space do less
+            with more, so that will be a conversation piece can you rework to make
+            the pizza look more delicious other agencies charge much lesser can
+            you make the blue bluer?. I think we need to start from scratch can my
+            website be in english?, yet make it sexy i&apos;ll pay you in a week
+            we don&apos;t need to pay upfront i hope you understand can you make
+            it stand out more?. Make the font bigger can you help me out? you will
+            get a lot of free exposure doing this that&apos;s going to be a chunk
+            of change other agencies charge much lesser. Are you busy this
+            weekend? I have a new project with a tight deadline that&apos;s going
+            to be a chunk of change. There are more projects lined up charge extra
+            the next time.
+            </Typography>
+        </div>
+    );
+  };
+  
+  export default Home;
+
+
+
+{/* <div className="w-full h-full text-left text-xs text-white font-inter">
+        <img
+          className="absolute top-[0px] left-[0px] w-[764px] h-[787px] object-cover"
+          alt=""
+          src="/bodydiv@2x.png"
+        />
+        <div className="absolute top-[584px] left-[0px] w-[764px] h-[203px]">
+          <div className="absolute top-[0px] left-[0px] bg-gray w-[764px] h-[203px]" />
+          <img
+            className="absolute top-[52px] left-[39px] rounded-8xs w-[100px] h-[100px] object-cover"
+            alt=""
+            src="/footerlogo@2x.png"
+          />
+          <div className="absolute top-[25px] left-[249px] w-[468px] h-[122px]">
+            <b className="absolute top-[0px] left-[0px] inline-block w-[118px] h-[122px]">
+              <p className="m-0">About</p>
+              <p className="m-0">&nbsp;</p>
+              <p className="m-0">&nbsp;</p>
+              <p className="m-0">&nbsp;</p>
+            </b>
+            <b className="absolute top-[0px] left-[168px] inline-block w-[118px] h-[122px]">
+              <p className="m-0">Services</p>
+              <p className="m-0">&nbsp;</p>
+            </b>
+            <b className="absolute top-[0px] left-[351px] inline-block w-[117px] h-[122px]">
+              <p className="m-0">Follow us on:</p>
+              <p className="m-0">&nbsp;</p>
+              <p className="m-0">&nbsp;</p>
+            </b>
+          </div>
+          <div className="absolute top-[170px] left-[0px] font-light text-center inline-block w-[764px]">
+            ToDo © Copyright 2023
+          </div>
+        </div>
+        <div className="absolute top-[18px] left-[6px] w-[751px] h-[86px]">
+          <div className="absolute top-[0px] left-[0px] w-[751px] h-[86px]" />
+          <img
+            className="absolute top-[19px] left-[688px] rounded-[50%] w-[50px] h-[50px] object-cover"
+            alt=""
+            src="/profilediv@2x.png"
+          />
+          <div className="absolute top-[29px] left-[292px] flex flex-row items-start justify-start gap-[13px]">
+            <div className="rounded-8xs bg-black shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] flex flex-row items-center justify-center py-2 px-[22px]">
+              <b className="relative">Home</b>
+            </div>
+            <div className="rounded-8xs bg-black shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] flex flex-row items-center justify-center py-2 px-[22px]">
+              <b className="relative">Tasks</b>
+            </div>
+            <div className="rounded-8xs bg-black shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] flex flex-row items-center justify-center py-2 px-[22px]">
+              <b className="relative">Shared</b>
+            </div>
+            <div className="rounded-8xs bg-black shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] flex flex-row items-center justify-center py-2 px-[22px]">
+              <b className="relative">Report</b>
+            </div>
+          </div>
+          <img
+            className="absolute top-[19px] left-[9px] rounded-8xs w-[161px] h-[49px] object-cover"
+            alt=""
+            src="/logodiv@2x.png"
+          />
+        </div>
+        <div className="absolute top-[179px] left-[139px] w-[486px] h-[215px] text-center text-[20px]">
+          <div className="absolute top-[0px] left-[0px] bg-lightcoral [backdrop-filter:blur(4px)] w-[486px] h-[215px]" />
+          <div className="absolute top-[50px] left-[64px] flex items-center w-[355px] h-[99px]">
+            <span className="[line-break:anywhere] w-full">
+              <p className="m-0">
+                <b>
+                  <span>WELCOME TO THE TODO APP!</span>
+                </b>
+              </p>
+              <p className="m-0 text-xs">
+                <span>
+                  <b>&nbsp;</b>
+                </span>
+              </p>
+              <p className="m-0 text-black">
+                <span>
+                  <span className="font-light">
+                    List all your TODOs in one place and get grip of the daily
+                    work schedules and fulfill the targets set by you.
+                  </span>
+                </span>
+              </p>
+            </span>
+          </div>
+        </div>
+      </div>
+  
+*/}
